ff916c6e54189f7be37efaaa39dc4c997c72e8cf
ikarus <leonard.moritz.huebner@campus.tu-berlin.de>
2023-11-13 05:42:51 +0100

    removing psi helper subtype

--- HML/HML.thy
+++ HML/HML.thy
@@ -8,11 +8,8 @@ datatype
     HML_poss 'act "('act, 'i) HML" |
     HML_silent "('act, 'i) HML" |
     HML_internal "('act, 'i) HML" |
-    HML_conj "'i set" "'i \<Rightarrow> ('act, 'i) HML_neg"
-and
-  ('act, 'i) HML_neg =
-    HML_just "('act, 'i) HML" |
-    HML_not "('act, 'i) HML"
+    HML_conj "'i set" "'i \<Rightarrow>  (('act, 'i) HML, ('act, 'i) HML) sum"
+
 
 context LTS_Tau
 begin
@@ -22,51 +19,33 @@ fun hml_semantic :: "('a, 's) HML \<Rightarrow> 's set" ("\<lbrakk>_\<rbrakk>HML
   "\<lbrakk>HML_true\<rbrakk>HML = {}" (* How to get the set of all processes? 's set? *)
 \<close>
 
-function
-      hml_models     :: "('a, 's) HML     \<Rightarrow> 's \<Rightarrow> bool" ("_ \<Turnstile> _" 60) 
-  and hml_neg_models :: "('a, 's) HML_neg \<Rightarrow> 's \<Rightarrow> bool"
+function hml_models     :: "('a, 's) HML     \<Rightarrow> 's \<Rightarrow> bool" ("_ \<Turnstile> _" 60) 
 where
   "(HML_true         \<Turnstile> _) = True" |
   "((HML_poss a \<phi>)   \<Turnstile> p) = (\<exists>p'. p \<mapsto> a p' \<and> (\<phi> \<Turnstile> p'))" |
   "((HML_silent \<phi>)   \<Turnstile> p) = (\<exists>p'. p \<Zsurj> p' \<and> (\<phi> \<Turnstile> p'))" |
   "((HML_internal \<phi>) \<Turnstile> p) = ((\<exists>p'. p \<mapsto> \<tau> p' \<and> (\<phi> \<Turnstile> p')) \<or> (\<phi> \<Turnstile> p))" |
-  "((HML_conj I \<psi>s)  \<Turnstile> p) = (\<forall>i \<in> I. hml_neg_models (\<psi>s i) p)" |
-
-  "(hml_neg_models (HML_just \<phi>) p) = (\<phi> \<Turnstile> p)" |
-  "(hml_neg_models (HML_not \<phi>) p) = (\<not>(\<phi> \<Turnstile> p))"
-                      apply (metis HML.exhaust HML_neg.exhaust sumE surj_pair)
-                      apply simp
-                      apply blast
-                      apply fastforce
-                      apply simp
-                      apply force
-                      apply blast
-                      apply force
-                      apply force
-                     apply force
-                    apply blast
-                   apply force
-                  apply fastforce
-                 apply force
+  "((HML_conj I \<psi>s)  \<Turnstile> p) = (\<forall>i \<in> I. (\<lambda>x. case x of Inl tr \<Rightarrow> (tr \<Turnstile> p) | Inr fl \<Rightarrow> \<not>(fl \<Turnstile> p)) (\<psi>s i))" 
+                 apply (metis HML.exhaust surj_pair)
                 apply simp
-               apply blast
-              apply blast
-             apply fastforce
+               apply simp
+              apply fastforce
+             apply blast
             apply simp
-           apply simp
+           apply fastforce
           apply fastforce
-         apply fastforce
-        apply force
-       apply simp
-      apply force
+         apply force
+        apply fastforce
+       apply fastforce
+      apply simp
      apply fastforce
-    apply simp
+    apply force
    apply simp
   by fastforce
 
 termination
 proof
-
+ 
 end
 
 end
