186b6d4d1acf027bb0008302b042545732d689f8
ekeln <karl.mattes36@gmail.com>
2023-11-13 12:07:39 +0100

    wellfoundedness proof approach

--- Hennessy_Milner_Logic.thy
+++ Hennessy_Milner_Logic.thy
@@ -40,7 +40,80 @@ inductive_set wf_rel_formula :: "(('s \<times> ('a, 's) hml_formula) + ('s \<tim
 lemma wf_rel_formula_wf: "wf wf_rel_formula"
   unfolding wf_def
 proof safe
-  show "\<And>P x. \<forall>x. (\<forall>y. (y, x) \<in> wf_rel_formula \<longrightarrow> P y) \<longrightarrow> P x \<Longrightarrow> P x" sorry
+  fix P p x
+  assume  A1 :"\<forall>x. (\<forall>y. (y, x) \<in> wf_rel_formula \<longrightarrow> P y) \<longrightarrow> P x"
+  then show "P x"
+  proof(cases x)
+    case (Inl a)
+    then obtain p \<phi> where "x = Inl (p, \<phi>)"
+      by fastforce
+    then have "P x = P (Inl (p, \<phi>))" by simp
+    from A1 have A2:
+"\<forall>p \<phi>. (\<forall>p' \<phi>'.
+(Inl (p',\<phi>'), Inl (p,\<phi>)) \<in> wf_rel_formula \<longrightarrow> P (Inl (p', \<phi>'))) \<and> 
+(\<forall>p' \<phi>'. (Inr (p',\<phi>'), Inl (p,\<phi>)) \<in> wf_rel_formula \<longrightarrow> P (Inr (p', \<phi>'))) 
+\<longrightarrow> P (Inl (p, \<phi>))"
+    using surj_pair
+    by (metis obj_sumE)
+  from A1 have A3:
+"\<forall>p \<phi>. (\<forall>p' \<phi>'.
+(Inl (p',\<phi>'), Inr (p,\<phi>)) \<in> wf_rel_formula \<longrightarrow> P (Inl (p', \<phi>'))) \<and> 
+(\<forall>p' \<phi>'. (Inr (p',\<phi>'), Inr (p,\<phi>)) \<in> wf_rel_formula \<longrightarrow> P (Inr (p', \<phi>'))) 
+\<longrightarrow> P (Inr (p, \<phi>))"
+    using surj_pair
+    by (metis obj_sumE)
+  with A2 have "P (Inl (p, \<phi>))"
+  proof(induct \<phi> arbitrary: p)
+    case HML_true
+    with wf_rel_formula.cases have "P (Inl (p, HML_true))" 
+      by (smt (verit) Inl_inject Inr_not_Inl Pair_inject hml_formula.distinct(1) hml_formula.distinct(3) hml_formula.distinct(5) hml_formula.distinct(7))
+    with HML_true(1) show ?case by simp
+  next
+    case (HML_conj x1 x2)
+    then show ?case sorry
+  next
+    case (HML_poss \<alpha> \<psi>)
+    with wf_rel_formula.cases hml_formula.distinct(4, 10, 15, 17) hml_formula.inject range_eqI
+    show "P (Inl (p, HML_poss \<alpha> \<psi>))" 
+      by (smt (verit) Inr_not_Inl snd_conv sum.sel(1))
+  next
+    case (HML_tau \<phi>)
+    with wf_rel_formula.cases hml_formula.distinct hml_formula.inject show ?case
+      by (smt (verit) Inl_inject Inr_not_Inl Pair_inject)
+  next
+    case (HML_eps \<phi>)
+    with wf_rel_formula.cases hml_formula.distinct hml_formula.inject show ?case 
+      by (smt (verit) Inl_inject Inr_not_Inl Pair_inject)
+  next
+    case (HML_neg \<psi>)
+    with wf_rel_formula.cases hml_psi.distinct hml_psi.inject
+    show ?thesis sorry
+  next
+    case (HML_phi \<psi>)
+    then show ?thesis sorry
+  qed
+  next
+    case (Inr b)
+    from A1 have A3:"\<forall>p \<phi>. (\<forall>p' \<phi>'.(Inl (p',\<phi>'), Inr (p,\<phi>)) \<in> wf_rel_formula \<longrightarrow> P (Inl (p', \<phi>'))) \<and> (\<forall>p' \<phi>'. (Inr (p',\<phi>'), Inr (p,\<phi>)) \<in> wf_rel_formula \<longrightarrow> P (Inr (p', \<phi>'))) \<longrightarrow> P (Inr (p, \<phi>))"
+    using surj_pair
+    by (metis obj_sumE)
+    then show ?thesis sorry
+  qed
+(*TODO: cases von assume betrachten, je nachdem ob x und y mit Inl/Inr constructed sind.*)
+  hence A2 :"\<forall>p \<phi>. (\<forall>p' \<phi>'.(Inl (p',\<phi>'), Inl (p,\<phi>)) \<in> wf_rel_formula \<longrightarrow> P (Inl (p', \<phi>'))) \<and> (\<forall>p' \<phi>'. (Inr (p',\<phi>'), Inl (p,\<phi>)) \<in> wf_rel_formula \<longrightarrow> P (Inr (p', \<phi>'))) \<longrightarrow> P (Inl (p, \<phi>))"
+    using surj_pair
+    by (metis obj_sumE)
+  from A1 have A3:"\<forall>p \<phi>. (\<forall>p' \<phi>'.(Inl (p',\<phi>'), Inr (p,\<phi>)) \<in> wf_rel_formula \<longrightarrow> P (Inl (p', \<phi>'))) \<and> (\<forall>p' \<phi>'. (Inr (p',\<phi>'), Inr (p,\<phi>)) \<in> wf_rel_formula \<longrightarrow> P (Inr (p', \<phi>'))) \<longrightarrow> P (Inr (p, \<phi>))"
+    using surj_pair
+    by (metis obj_sumE)
+  from A2 have A2: "P (Inl (p, \<phi>))" sorry
+  from A3 have A3: "P (Inr (q, \<psi>))" sorry
+  from A2 A3 show ?thesis
+hence "\<forall>p \<phi>. (\<forall>p' \<phi>'. ((p',\<phi>'), (p,\<phi>)) \<in> wf_rel_formula \<longrightarrow> P (case_sum (\<lambda>(p', \<phi>'). P (p', \<phi>')) (\<lambda>(p', \<phi>'). P (p', \<phi>')) (Inl (p', \<phi>')))) \<longrightarrow>
+                 P (case_sum (\<lambda>(p', \<phi>'). P (p', \<phi>')) (\<lambda>(p', \<phi>'). P (p', \<phi>')) (Inl (p, \<phi>)))"
+  hence "\<forall>p \<phi>. (\<forall>p' \<phi>'. ((p',\<phi>'), (p,\<phi>)) \<in> wf_rel_formula \<longrightarrow> P (case_sum (\<lambda>(p', \<phi>'). P (p', \<phi>')) (\<lambda>(p', \<phi>'). P (p', \<phi>')) (Inl (p, \<phi>')))) \<longrightarrow> P (case_sum (\<lambda>(p', \<phi>'). P (p', \<phi>')) (\<lambda>(p', \<phi>'). P (p', \<phi>')) (Inl (p, \<phi>)))"
+  hence "\<forall>p \<phi>. (\<forall>p' \<phi>'. ((p',\<phi>'), (p,\<phi>)) \<in> wf_rel_formula \<longrightarrow> P (p',\<phi>')) \<longrightarrow> P (p, \<phi>)"
+  sorry
 qed
 
 termination
