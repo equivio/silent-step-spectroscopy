51d035971acedf519143dfde08930ea39f2c24bf
ekeln <karl.mattes36@gmail.com>
2023-11-23 12:14:34 +0100

    adjusted sr_depth function, does this terminate?

--- Price_Spectra/test_srbb.thy
+++ Price_Spectra/test_srbb.thy
@@ -9,21 +9,25 @@ begin
 context LTS_Tau
 begin
 
-function sr_depth:: "('a, 'i)HML \<Rightarrow> nat" and
-  sr_depth_chi:: "('a, 'i)HML_neg \<Rightarrow> nat" where
+function sr_depth:: "('a, 'i)HML \<Rightarrow> enat" and
+  sr_depth_chi:: "('a, 'i)HML_neg \<Rightarrow> enat" where
 "sr_depth HML_true = 0" |
 "sr_depth (HML_poss _ \<phi>) = sr_depth \<phi>" |
 "sr_depth (HML_silent \<phi>) = sr_depth \<phi>" |
 "sr_depth (HML_internal \<phi>) = sr_depth \<phi>" |
-"sr_depth (HML_conj I F) = Max ({0} \<union> {sr_depth_chi(F i)|i. i \<in> I})" |
-"sr_depth_chi (HML_just (HML_internal \<phi>)) = 1 + sr_depth \<phi>" |
-"sr_depth_chi (HML_just \<phi>) = sr_depth \<phi>" |
+"sr_depth (HML_conj I F) = (if finite {sr_depth_chi(F i)|i. i \<in> I} then Max ({0} \<union> {sr_depth_chi(F i)|i. i \<in> I})
+else \<infinity>)" |
+"sr_depth_chi (HML_just \<phi>) = (if (\<exists>\<psi>. \<phi> = HML_internal \<psi>) then 1 + sr_depth \<phi> else sr_depth \<phi>)" |
 "sr_depth_chi (HML_not \<phi>) = sr_depth \<phi>"
 
-                      apply (metis HML.exhaust HML_neg.exhaust obj_sumE)
-                      apply simp+
-  sorry
+  by ((metis HML.exhaust HML_neg.exhaust obj_sumE), simp+)
+
 termination sorry
+
+(*The nat set has a supremum in nat*)
+definition has_supr :: "nat set \<Rightarrow> bool" where
+  "has_supr S \<equiv> (\<exists>sup. (\<forall>x \<in> S. x \<le> sup) \<and> (\<forall>y. \<forall>x \<in> S. x \<le> y \<longrightarrow> sup \<le> y))"
+
 
 fun sr_depth_srbb:: "('a, 'i)HML \<Rightarrow> enat" where
 "sr_depth_srbb \<phi> = (if is_hml_srbb \<phi> then sr_depth \<phi> else \<infinity>)"
