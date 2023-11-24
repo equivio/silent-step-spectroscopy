6fcce0712468af198273641095c5686d9ef16c84
TheHllm <hllmollenkopf@gmail.com>
2023-11-24 13:40:13 +0100

    Added lemma that rewrites pairs via zip

--- EnergyGames.thy
+++ EnergyGames.thy
@@ -94,6 +94,10 @@ lemma finite_play_min_len: "finite_play p \<Longrightarrow> length p \<ge> 1"
 fun pairs :: "'a list \<Rightarrow> ('a \<times> 'a) list" where
   "pairs p = (if length p \<le> 1 then [] else (hd p, hd (tl p)) # pairs (tl p))"
 
+lemma pairs_is_zip:
+  shows "pairs p \<equiv> zip (p) (tl p)"
+  by (induct p, simp_all, smt (verit, ccfv_threshold) One_nat_def Suc_lessI energy_game.pairs.elims length_0_conv length_Suc_conv length_greater_0_conv linorder_not_less list.collapse list.sel(3) nat_neq_iff zip.simps(1) zip_Cons_Cons)
+
 (* some intuition on this definition*)
 lemma "pairs [1,2,3] = [(1,2), (2,3)]" by simp
 lemma empty_pair: "pairs [] = []" by simp
