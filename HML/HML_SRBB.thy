9bf0513b6bb38a23a404273a8980099423bdd246
ikarus <leonard.moritz.huebner@campus.tu-berlin.de>
2023-12-05 12:55:38 +0100

    moving TT data constructor in hml srbb

--- HML/HML_SRBB.thy
+++ HML/HML_SRBB.thy
@@ -4,7 +4,6 @@ begin
 
 datatype 
   ('act, 'i) hml_srbb =
-    TT |
     Silent "('act, 'i) hml_srbb_conjunction" |
     ImmConj "'i set" "'i \<Rightarrow> ('act, 'i) hml_srbb_conjunct"
 and
@@ -13,17 +12,17 @@ and
     Conj "'i set" "'i \<Rightarrow> ('act, 'i) hml_srbb_conjunct" |
     StableConj "'i set" "'i \<Rightarrow> ('act, 'i) hml_srbb_conjunct" |
     BranchConj 'act "('act, 'i) hml_srbb"
-                         "'i set" "'i \<Rightarrow> ('act, 'i) hml_srbb_conjunct"
+               "'i set" "'i \<Rightarrow> ('act, 'i) hml_srbb_conjunct"
 and
   ('act, 'i) hml_srbb_conjunct =
     Pos "('act, 'i) hml_srbb_conjunction" |
-    Neg "('act, 'i) hml_srbb_conjunction"
+    Neg "('act, 'i) hml_srbb_conjunction" |
+    TT
 
 primrec
       modal_depth_srbb :: "('act, 'i) hml_srbb \<Rightarrow> nat"
   and modal_depth_srbb_conjunction :: "('act, 'i) hml_srbb_conjunction \<Rightarrow> nat"
   and modal_depth_srbb_conjunct :: "('act, 'i) hml_srbb_conjunct \<Rightarrow> nat" where
- "modal_depth_srbb TT = 0" |
  "modal_depth_srbb (Silent \<chi>) = modal_depth_srbb_conjunction \<chi>" |
  "modal_depth_srbb (ImmConj I \<psi>s) = Sup ({0} \<union> {modal_depth_srbb_conjunct (\<psi>s i) | i. i \<in> I})" |
 
@@ -36,17 +35,20 @@ primrec
     Sup ({1 + modal_depth_srbb \<phi>} \<union> {modal_depth_srbb_conjunct (\<psi>s i) | i. i \<in> I})" |
 
  "modal_depth_srbb_conjunct (Pos \<chi>) = modal_depth_srbb_conjunction \<chi>" |
- "modal_depth_srbb_conjunct (Neg \<chi>) = modal_depth_srbb_conjunction \<chi>"
+ "modal_depth_srbb_conjunct (Neg \<chi>) = modal_depth_srbb_conjunction \<chi>" |
+ "modal_depth_srbb_conjunct TT = 0"
   
 
 context Inhabited_Tau_LTS
 begin
 
+abbreviation HML_srbb_true :: "('a, 's) hml_srbb" where
+  "HML_srbb_true \<equiv> ImmConj {l} (\<lambda>_. TT)"
+
 primrec
       hml_srbb_to_hml :: "('a, 's) hml_srbb \<Rightarrow> ('a, 's) hml"
   and hml_srbb_conjunction_to_hml :: "('a, 's) hml_srbb_conjunction \<Rightarrow> ('a, 's) hml"
   and hml_srbb_conjunct_to_hml_neg :: "('a, 's) hml_srbb_conjunct \<Rightarrow> ('a, 's) hml_conjunct" where
-  "hml_srbb_to_hml TT = hml.TT" |
   "hml_srbb_to_hml (Silent \<chi>) = hml.Silent (hml_srbb_conjunction_to_hml \<chi>)" |
   "hml_srbb_to_hml (ImmConj I \<psi>s) = hml.Conj I (hml_srbb_conjunct_to_hml_neg \<circ> \<psi>s)" |
 
@@ -62,15 +64,15 @@ primrec
       \<and>hml hml_conjunct.Pos (hml.Conj I (hml_srbb_conjunct_to_hml_neg \<circ> \<psi>s)))" |
 
   "hml_srbb_conjunct_to_hml_neg (Pos \<chi>) = hml_conjunct.Pos (hml_srbb_conjunction_to_hml \<chi>)" |
-  "hml_srbb_conjunct_to_hml_neg (Neg \<chi>) = hml_conjunct.Neg (hml_srbb_conjunction_to_hml \<chi>)"
-
+  "hml_srbb_conjunct_to_hml_neg (Neg \<chi>) = hml_conjunct.Neg (hml_srbb_conjunction_to_hml \<chi>)" |
+  "hml_srbb_conjunct_to_hml_neg TT      = hml_conjunct.Pos hml.TT"
 
 fun hml_srbb_models :: "('a, 's) hml_srbb \<Rightarrow> 's \<Rightarrow> bool" ("_ \<Turnstile>SRBB _" 60)where
   "hml_srbb_models formula state = ((hml_srbb_to_hml formula) \<Turnstile> state)"
 
-lemma "(TT \<Turnstile>SRBB state) = (ImmConj {} \<psi>s \<Turnstile>SRBB state)"
+lemma "(HML_srbb_true \<Turnstile>SRBB state) = (ImmConj {} \<psi>s \<Turnstile>SRBB state)"
   by simp
 
-end
+end (* Inhabited_Tau_LTS *)
 
 end
