fbcb7ae1074b61549e21387c85cc9c9b72bd5f40
TheHllm <hllmollenkopf@gmail.com>
2023-12-08 14:32:01 +0100

    Revert "added eneg"
    This reverts commit 8f48948d254117ce459532cee7648ef4c3cda59d.

--- Energy.thy
+++ Energy.thy
@@ -3,29 +3,20 @@ theory Energy
 begin
 
 datatype energy = E (one: "enat") (two: "enat") (three: "enat") (four: "enat")
-                    (five: "enat") (six: "enat") (seven: "enat") (eight: "enat") | eneg
+                    (five: "enat") (six: "enat") (seven: "enat") (eight: "enat")
 
 instantiation energy :: minus
 begin
-abbreviation "larger_or_eq e1 e2 \<equiv> ((one e1) \<ge> (one e2)) \<and> ((two e1) \<ge> (two e2))
-                           \<and> ((three e1) \<ge> (three e2))\<and> ((four e1) \<ge> (four e2))
-                           \<and> ((five e1) \<ge> (five e2))\<and> ((six e1) \<ge> (six e2))
-                           \<and> ((seven e1) \<ge> (seven e2))\<and> ((eight e1) \<ge> (eight e2))"
-
-definition minus_energy_def: "e1 - e2 \<equiv> if (larger_or_eq e1 e2) then 
-                                          E ((one e1) - (one e2)) ((two e1) - (two e2)) 
-                                            ((three e1) - (three e2)) ((four e1) - (four e2)) 
-                                            ((five e1) - (five e2)) ((six e1) - (six e2))
-                                            ((seven e1) - (seven e2)) ((eight e1) - (eight e2))
-                                         else eneg"
+definition minus_energy_def: "e1 - e2 \<equiv> E ((one e1) - (one e2)) ((two e1) - (two e2)) ((three e1) - (three e2))
+                                          ((four e1) - (four e2)) ((five e1) - (five e2)) ((six e1) - (six e2))
+                                          ((seven e1) - (seven e2)) ((eight e1) - (eight e2))"
 
 instance ..
 
 lemma energy_minus[simp]:
-  assumes " larger_or_eq (E a1 b1 c1 d1 e1 f1 g1 h1) ( E a2 b2 c2 d2 e2 f2 g2 h2)"
   shows "E a1 b1 c1 d1 e1 f1 g1 h1 - E a2 b2 c2 d2 e2 f2 g2 h2 = E (a1 - a2) (b1 - b2) (c1 - c2) (d1 - d2) 
           (e1 - e2) (f1 - f2) (g1 -g2) (h1 - h2)" 
-  using minus_energy_def assms by auto
+  by (simp add: minus_energy_def)
 end
 
 definition "min1_6 e \<equiv> E (min (one e) (six e)) (two e) (three e) (four e) (five e) (six e) (seven e) (eight e)"
