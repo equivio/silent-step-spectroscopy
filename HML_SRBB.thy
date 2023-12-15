2c44f4ef3db168a5a0a8c74d67ae0dabf4102a09
ikarus <leonard.moritz.huebner@campus.tu-berlin.de>
2023-12-15 17:33:21 +0100

    state theorem showing validity of transformation

--- HML_SRBB.thy
+++ HML_SRBB.thy
@@ -119,4 +119,44 @@ inductive tau_obs_free :: "('act, 'i) hml_srbb \<Rightarrow> bool"
   "tau_obs_free_\<psi> (Pos \<chi>)" if "tau_obs_free_\<chi> \<chi>" |
   "tau_obs_free_\<psi> (Neg \<chi>)" if "tau_obs_free_\<chi> \<chi>"
 
+context Inhabited_Tau_LTS
+begin
+
+primrec
+      hml_srbb_to_hml' :: "('a, 's) hml_srbb \<Rightarrow> ('a, 's) hml"
+  and hml_srbb_conjunction_to_hml' :: "('a, 's) hml_srbb_conjunction \<Rightarrow> ('a, 's) hml"
+  and hml_srbb_conjunct_to_hml_conjunct' :: "('a, 's) hml_srbb_conjunct \<Rightarrow> ('a, 's) hml_conjunct" where
+  "hml_srbb_to_hml' TT = hml.TT" |
+  "hml_srbb_to_hml' (Internal \<chi>) = hml.Internal (hml_srbb_conjunction_to_hml' \<chi>)" |
+  "hml_srbb_to_hml' (ImmConj I \<psi>s) = hml.Conj I (hml_srbb_conjunct_to_hml_conjunct' \<circ> \<psi>s)" |
+
+  "hml_srbb_conjunction_to_hml' (Obs a \<phi>) = HML_soft_poss a (hml_srbb_to_hml' \<phi>)" |
+  "hml_srbb_conjunction_to_hml' (Conj I \<psi>s) = hml.Conj I (hml_srbb_conjunct_to_hml_conjunct' \<circ> \<psi>s)" |
+
+  "hml_srbb_conjunction_to_hml' (StableConj I \<psi>s) =
+    (hml_conjunct.Neg (hml.Obs \<tau> hml.TT)
+     \<and>hml hml_conjunct.Pos (hml.Conj I (hml_srbb_conjunct_to_hml_conjunct' \<circ> \<psi>s)))" |
+
+  "hml_srbb_conjunction_to_hml' (BranchConj a \<phi> I \<psi>s) = 
+     (hml_conjunct.Pos (HML_soft_poss a (hml_srbb_to_hml' \<phi>))
+      \<and>hml hml_conjunct.Pos (hml.Conj I (hml_srbb_conjunct_to_hml_conjunct' \<circ> \<psi>s)))" |
+
+  "hml_srbb_conjunct_to_hml_conjunct' (Pos \<chi>) = hml_conjunct.Pos (hml.Internal (hml_srbb_conjunction_to_hml' \<chi>))" |
+  "hml_srbb_conjunct_to_hml_conjunct' (Neg \<chi>) = hml_conjunct.Neg (hml.Internal (hml_srbb_conjunction_to_hml' \<chi>))"
+
+lemma
+  fixes \<phi> :: "('a, 's) hml_srbb"
+    and \<chi> :: "('a, 's) hml_srbb_conjunction"
+    and \<psi>
+  assumes "tau_obs_free \<phi>"
+    and "tau_obs_free_\<chi> \<chi>"
+    and "tau_obs_free_\<psi> \<psi>"
+  shows "((state \<Turnstile> (hml_srbb_to_hml \<phi>)) \<longleftrightarrow> (state \<Turnstile> (hml_srbb_to_hml' \<phi>)))
+        \<and> ((state' \<Turnstile> (hml_srbb_conjunction_to_hml \<chi>)) \<longleftrightarrow> (state' \<Turnstile> (hml_srbb_conjunction_to_hml' \<chi>)))
+        \<and> ((hml_conjunct_models state'' (hml_srbb_conjunct_to_hml_conjunct \<psi>)) \<longleftrightarrow> (hml_conjunct_models state'' (hml_srbb_conjunct_to_hml_conjunct' \<psi>)))"
+  sorry
+
+  
+end
+
 end
