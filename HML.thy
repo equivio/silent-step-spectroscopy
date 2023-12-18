fc90619805254c425b9393901b450939e680dc9e
ikarus <leonard.moritz.huebner@campus.tu-berlin.de>
2023-12-18 09:35:52 +0100

    adding formula equivalence and proving some single step congrunece properties

--- HML.thy
+++ HML.thy
@@ -71,5 +71,99 @@ lemma "(state \<Turnstile> \<phi>) = (state \<Turnstile> HML_not (HML_not \<phi>
 
 end (* context Inhabited_Tau_LTS *)
 
+context LTS_Tau
+begin
+
+definition hml_impl :: "('a, 's) hml \<Rightarrow> ('a, 's) hml \<Rightarrow> bool" (infix "\<Rrightarrow>" 60)  where
+  "\<phi>l \<Rrightarrow> \<phi>r \<equiv> (\<forall>p. (p \<Turnstile> \<phi>l) \<longrightarrow> (p \<Turnstile> \<phi>r))"
+
+lemma hml_impl_preord: "reflp (\<Rrightarrow>) \<and> transp (\<Rrightarrow>)"
+  by (metis hml_impl_def reflpI transpI)
+
+definition hml_conjunct_impl :: "('a, 's) hml_conjunct \<Rightarrow> ('a, 's) hml_conjunct \<Rightarrow> bool" (infix "\<and>\<Rrightarrow>" 60)  where
+  "\<psi>l \<and>\<Rrightarrow> \<psi>r \<equiv> (\<forall>p. (hml_conjunct_models p \<psi>l) \<longrightarrow> (hml_conjunct_models p \<psi>r))"
+
+lemma hml_conjunct_impl_preord: "reflp (\<and>\<Rrightarrow>) \<and> transp (\<and>\<Rrightarrow>)"
+  by (metis hml_conjunct_impl_def reflpI transpI)
+
+definition hml_eq :: "('a, 's) hml \<Rightarrow> ('a, 's) hml \<Rightarrow> bool" (infix "\<Lleftarrow>\<Rrightarrow>" 60)  where
+  "\<phi>l \<Lleftarrow>\<Rrightarrow> \<phi>r \<equiv> \<phi>l \<Rrightarrow> \<phi>r \<and> \<phi>r \<Rrightarrow> \<phi>l"
+
+lemma hml_eq_equiv: "equivp (\<Lleftarrow>\<Rrightarrow>)"
+  by (smt (verit, del_insts) equivpI hml_eq_def hml_impl_def reflpI sympI transpI)
+
+definition hml_conjunct_eq :: "('a, 's) hml_conjunct \<Rightarrow> ('a, 's) hml_conjunct \<Rightarrow> bool" (infix "\<Lleftarrow>\<and>\<Rrightarrow>" 60)  where
+  "\<psi>l \<Lleftarrow>\<and>\<Rrightarrow> \<psi>r \<equiv> \<psi>l \<and>\<Rrightarrow> \<psi>r \<and> \<psi>r \<and>\<Rrightarrow> \<psi>l"
+
+lemma hml_conjunct_eq_equiv: "equivp (\<Lleftarrow>\<and>\<Rrightarrow>)"
+  by (smt (verit, best) equivpI hml_conjunct_eq_def hml_conjunct_impl_def reflpI sympI transpI)
+
+\<comment> \<open> Congruence Properties\<close>
+
+lemma obs_cong: "\<phi>l \<Lleftarrow>\<Rrightarrow> \<phi>r \<Longrightarrow> (Obs \<alpha> \<phi>l) \<Lleftarrow>\<Rrightarrow> (Obs \<alpha> \<phi>r)"
+  by (meson hml_eq_def hml_impl_def hml_models.simps(2))
+
+lemma internal_cong: "\<phi>l \<Lleftarrow>\<Rrightarrow> \<phi>r \<Longrightarrow> (Internal \<phi>l) \<Lleftarrow>\<Rrightarrow> (Internal \<phi>r)"
+  using hml_eq_def hml_impl_def by auto
+
+lemma silent_cong: "\<phi>l \<Lleftarrow>\<Rrightarrow> \<phi>r \<Longrightarrow> (Silent \<phi>l) \<Lleftarrow>\<Rrightarrow> (Silent \<phi>r)"
+  using hml_eq_def hml_impl_def by auto
+
+lemma conj_cong: "\<psi>sl ` I = \<psi>sr ` I \<Longrightarrow> (\<psi>sl l) \<Lleftarrow>\<and>\<Rrightarrow> (\<psi>sr r) \<Longrightarrow> (Conj (I \<union> {l}) \<psi>sl) \<Lleftarrow>\<Rrightarrow> (Conj (I \<union> {r}) \<psi>sr)"
+proof -
+  assume "\<psi>sl ` I = \<psi>sr ` I"
+     and "(\<psi>sl l) \<Lleftarrow>\<and>\<Rrightarrow> (\<psi>sr r)"
+  hence "(\<forall>p. hml_conjunct_models p (\<psi>sl l) \<longrightarrow> hml_conjunct_models p (\<psi>sr r))
+       \<and> (\<forall>p. hml_conjunct_models p (\<psi>sr r) \<longrightarrow> hml_conjunct_models p (\<psi>sl l))"
+    unfolding hml_conjunct_eq_def
+          and hml_conjunct_impl_def by auto
+  then have
+        IHL: "\<forall>p. hml_conjunct_models p (\<psi>sl l) \<longrightarrow> hml_conjunct_models p (\<psi>sr r)"
+    and IHR: "\<forall>p. hml_conjunct_models p (\<psi>sr r) \<longrightarrow> hml_conjunct_models p (\<psi>sl l)" 
+    apply blast 
+    using \<open>(\<forall>p. hml_conjunct_models p (\<psi>sl l) \<longrightarrow> hml_conjunct_models p (\<psi>sr r)) \<and> (\<forall>p. hml_conjunct_models p (\<psi>sr r) \<longrightarrow> hml_conjunct_models p (\<psi>sl l))\<close> by blast
+  
+  show "(Conj (I \<union> {l}) \<psi>sl) \<Lleftarrow>\<Rrightarrow> (Conj (I \<union> {r}) \<psi>sr)"
+    unfolding hml_eq_def
+          and hml_impl_def
+  proof (rule conjI)
+    show "\<forall>p. p \<Turnstile> Conj (I \<union> {l}) \<psi>sl \<longrightarrow> p \<Turnstile> Conj (I \<union> {r}) \<psi>sr"
+    proof (rule allI, rule impI)
+      fix p
+      assume "p \<Turnstile> Conj (I \<union> {l}) \<psi>sl"
+      hence "\<forall>i\<in>I \<union> {l}. hml_conjunct_models p (\<psi>sl i)"
+        unfolding hml_models.simps.
+      then have "hml_conjunct_models p (\<psi>sl l)"
+        by blast
+      then have "hml_conjunct_models p (\<psi>sr r)"
+        using IHL by simp
+      then show "p \<Turnstile> Conj (I \<union> {r}) \<psi>sr"
+        using \<open>\<psi>sl ` I = \<psi>sr ` I\<close> 
+        by (smt (verit) Un_insert_right \<open>\<forall>i\<in>I \<union> {l}. hml_conjunct_models p (\<psi>sl i)\<close> hml_models.simps(5) image_iff insert_iff sup_bot.right_neutral)
+    qed
+  next
+    show "\<forall>p. p \<Turnstile> Conj (I \<union> {r}) \<psi>sr \<longrightarrow> p \<Turnstile> Conj (I \<union> {l}) \<psi>sl"
+    proof (rule allI, rule impI)
+      fix p
+      assume "p \<Turnstile> Conj (I \<union> {r}) \<psi>sr"
+      hence "\<forall>i\<in>I \<union> {r}. hml_conjunct_models p (\<psi>sr i)"
+        unfolding hml_models.simps.
+      then have "hml_conjunct_models p (\<psi>sr r)"
+        by blast
+      then have "hml_conjunct_models p (\<psi>sl l)"
+        using IHR by simp
+      then show " p \<Turnstile> Conj (I \<union> {l}) \<psi>sl" 
+        by (smt (verit, best) Un_empty_right Un_insert_right \<open>\<forall>i\<in>I \<union> {r}. hml_conjunct_models p (\<psi>sr i)\<close> \<open>\<psi>sl ` I = \<psi>sr ` I\<close> hml_models.simps(5) image_iff insert_iff)
+    qed
+  qed
+qed
+
+lemma pos_cong: "\<phi>l \<Lleftarrow>\<Rrightarrow> \<phi>r \<Longrightarrow> (Pos \<phi>l) \<Lleftarrow>\<and>\<Rrightarrow> (Pos \<phi>r)"
+  by (simp add: hml_conjunct_eq_def hml_conjunct_impl_def hml_eq_def hml_impl_def)
+
+lemma neg_cong: "\<phi>l \<Lleftarrow>\<Rrightarrow> \<phi>r \<Longrightarrow> (Neg \<phi>l) \<Lleftarrow>\<and>\<Rrightarrow> (Neg \<phi>r)"
+  by (meson hml_conjunct_eq_def hml_conjunct_impl_def hml_conjunct_models.simps(2) hml_eq_def hml_impl_def)
+
+end
 
 end
