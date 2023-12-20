fe97ba00138e9e9b13779b89c21b32ea61c309d1
ikarus <leonard.moritz.huebner@campus.tu-berlin.de>
2023-12-20 21:17:37 +0100

    ensuring that functions are properly bound, explicit mentioning of inhabited tau lts is stinky

--- Silent_Step_Spectroscopy.thy
+++ Silent_Step_Spectroscopy.thy
@@ -1,8 +1,12 @@
 theory Silent_Step_Spectroscopy
-  imports Spectroscopy_Game Expressiveness_Price Weak_Traces
+  imports Spectroscopy_Game Expressiveness_Price HML_SRBB Weak_Traces
 begin
 
-theorem spectroscopy_game_correctness: "\<exists>\<phi> \<in> \<O> e. distinguishes_from \<phi> p Q = in_wina e (Attacker_Immediate p Q)"
+
+theorem (in full_spec_game) spectroscopy_game_correctness:
+  shows "(\<exists>\<phi> \<in> \<O> e. Inhabited_Tau_LTS.distinguishes_from step left right \<tau> \<phi> p Q)
+       = (in_wina e (Attacker_Immediate p Q))"
   oops
+
 
 end
