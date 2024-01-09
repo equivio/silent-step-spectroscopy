96e3ae51c333f69b52d77db3d0c053e562f34ef2
Caro <lemke.3@campus.tu-berlin.de>
2024-01-09 11:44:30 +0100

    Update Spectroscopy_Game.thy

--- Spectroscopy_Game.thy
+++ Spectroscopy_Game.thy
@@ -99,12 +99,12 @@ locale full_spec_game =
       \<tau> :: 'a
 begin
 
-
+(*
 text\<open>Next we show that the lemma win_a_upwards_closure ("with more energy the attacker will win at 
 least as much as before") applies to spectroscopy games. To apply the lemma we first show that  the 
 given order \<open>\<le>\<close> on energies fullfills the following properties:  
 - all updates are monotonic w.r.t. \<open>\<le>\<close> 
-- \<open>e \<le> Upd(e)\<close> holds for all energies and updates. \<close>
+- \<open>e \<le> Upd(e)\<close> holds for all energies and updates. \<close> *)
 
 lemma update_monotonicity: 
   fixes g g' e e'
