6cc2059983aa7d8b0cb06e0d3a971aad947921e6
Vervada <lisa.a.barthel@campus.tu-berlin.de>
2024-02-07 09:58:19 +0100

    merge first two texts

--- Energy_Games.thy
+++ Energy_Games.thy
@@ -6,9 +6,7 @@ begin
 
 text\<open>\noindent In this theory we introduce energy games and give basic definitions such as (winning) plays. 
 Energy games are the foundation for the later introduced full spectroscopy game, which is an 
-energy game itself, characterizing equivalence problems.\<close>
-
-text\<open>\noindent We use an abstract concept of energies and only later consider 8-dimensional energy games.
+energy game itself, characterizing equivalence problems.We use an abstract concept of energies and only later consider 8-dimensional energy games.
 In order to be able to combine this with our later definition of energies as a data type, there has to be a partial order for energies in our formalization.\<close>
 
 type_synonym 'energy update = "'energy \<Rightarrow> 'energy"
