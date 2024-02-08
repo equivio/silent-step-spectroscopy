5f61c9e765bc6a7bff721626c31f936591945324
TheHllm <hllmollenkopf@gmail.com>
2024-02-08 17:58:50 +0100

    Removed \noindent

--- Spectroscopy_Game.thy
+++ Spectroscopy_Game.thy
@@ -3,10 +3,10 @@ theory Spectroscopy_Game
   imports Energy_Games Energy LTS
 begin
 
-text \<open>\noindent In this theory we define the weak spectroscopy game as a locale.
+text \<open>In this theory we define the weak spectroscopy game as a locale.
 This game is an energy game constructed by adding stable and branching conjunctions to a delay bisimulation game that depends on an LTS.\<close>
 
-text \<open>\noindent We differentiate the positions accordingly and define the moves of the game corresponding to their names in \cite{bisping2023lineartimebranchingtime}.\<close>
+text \<open>We differentiate the positions accordingly and define the moves of the game corresponding to their names in \cite{bisping2023lineartimebranchingtime}.\<close>
 datatype ('s, 'a) spectroscopy_position = 
                           Attacker_Immediate (attacker_state: "'s") (defender_states: "'s set") |
                           Attacker_Branch (attacker_state: "'s") (defender_states: "'s set") |
@@ -89,7 +89,7 @@ fun spectroscopy_defender where
   "spectroscopy_defender (Defender_Stable_Conj _ _) = True"
 
 
-text \<open>\noindent In the following, we check whether our definitions are compatible with those of energy games.
+text \<open>In the following, we check whether our definitions are compatible with those of energy games.
 For this purpose we show the monotonicity of energy updates and that they are only decreasing.\<close>
 
 lemma update_monotonicity: 
@@ -321,7 +321,7 @@ qed
 
 end
 
-text \<open>\noindent Now we are able to define the weak spectroscopy game on an arbitrary (but inhabited) LTS.\<close>
+text \<open>Now we are able to define the weak spectroscopy game on an arbitrary (but inhabited) LTS.\<close>
 locale full_spec_game =
   Inhabited_Tau_LTS step left right \<tau>
   + energy_game "spectroscopy_moves" "spectroscopy_defender" "eneg" "less_eq"
